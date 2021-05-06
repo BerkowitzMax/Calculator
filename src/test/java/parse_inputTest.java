3f97c29131f4f14723169baffc9e2965cf09b8be
maxberko <maxberko@umich.edu>
2021-05-06 14:21:09 -0400

    split assertions up into different methods

--- src/test/java/parse_inputTest.java
+++ src/test/java/parse_inputTest.java
@@ -15,64 +15,74 @@ class parse_inputTest {
     }
 
     @Test
-    void parseExpression_test() {
-        String test_output = "yeet";
-        String expected = "yeet";
-        parse_input c = new parse_input();
+    void parseExpression_test1_basic() {
+        String expected = "1+1";
+        parse_input c = new parse_input("1+1");
+        String test_output = stringify(c.stackGetter());
 
-        // TEST 1
-        expected = "1+1";
-        c = new parse_input("1+1$");
-        test_output = stringify(c.stackGetter());
         assertEquals(expected, test_output);
-        System.out.println("T1_test passed.");
-
-        // Test 2
-        expected = "1*(2^3+4)";
-        c = new parse_input("1*(2^3+4)$");
-        test_output = stringify(c.stackGetter());
-        assertEquals(expected, test_output);
-        System.out.println("T2_test passed.");
-
-        // Test 3
-        expected = "1*(2^3+4)-(4%45/(22000.1))";
-        c = new parse_input("1*(2^3+4)-(4%45/(22,000.1))$");
-        test_output = stringify(c.stackGetter());
-        assertEquals(expected, test_output);
-        System.out.println("T3_test passed.");
-
-        // Test 4
-        expected = ".+.";
-        c = new parse_input(".+.$");
-        test_output = stringify(c.stackGetter());
-        assertEquals(expected, test_output);
-        System.out.println("T4_test passed.");
     }
 
     @Test
-    void parseExpression_test_invalid() {
-        // TEST 1
-        assertThrows(NullPointerException.class, () -> {
-            new parse_input("(1+1$");
-        });
-        System.out.println("T1_test_invalid passed.");
+    void parseExpression_test2_operators() {
+        String expected = "1*(2^3+4)";
+        parse_input c = new parse_input("1*(2^3+4)");
+        String test_output = stringify(c.stackGetter());
 
-        // TEST 2
-        assertThrows(NullPointerException.class, () -> {
-            new parse_input("(1+1)*((1.)$");
-        });
-        System.out.println("T2_test_invalid passed.");
+        assertEquals(expected, test_output);
 
-        // TEST 3
-        assertThrows(NullPointerException.class, () -> {
-            new parse_input("(1+1)*1..$");
-        });
-        System.out.println("T3_test_invalid passed.");
+        // Testing symbols and spaces
+        expected = "1*2";
+        c = new parse_input("1$$*    2");
+        test_output = stringify(c.stackGetter());
 
-        // TEST 4
+        assertEquals(expected, test_output);
+    }
+
+    @Test
+    void parseExpression_test3_complex() {
+        String expected = "1*(2^3+4)-(4%45/(22000.1))";
+        parse_input c = new parse_input("1*(2^3+4)-(4%45/(22,000.1))");
+        String test_output = stringify(c.stackGetter());
+
+        assertEquals(expected, test_output);
+    }
+
+    @Test
+    void parseExpression_test4_decimal() {
+        String expected = ".+.";
+        parse_input c = new parse_input(".+.");
+        String test_output = stringify(c.stackGetter());
+
+        assertEquals(expected, test_output);
+    }
+
+    @Test
+    void parseInvalidExpression_test1_parenthesis() {
         assertThrows(NullPointerException.class, () -> {
-            new parse_input("(1+1)*1.)$");
+            new parse_input("(1+1");
         });
-        System.out.println("T4_test_invalid passed.");
+
+        assertThrows(NullPointerException.class, () -> {
+            new parse_input("(1+1)*((1.)");
+        });
+
+        assertThrows(NullPointerException.class, () -> {
+            new parse_input("(1+1)*1.)");
+        });
+    }
+
+    @Test
+    void parseInvalidExpression_test2_decimal() {
+        assertThrows(NullPointerException.class, () -> {
+            new parse_input("(1+1)*1..");
+        });
+    }
+
+    @Test
+    void parseInvalidExpression_test3_operators() {
+        assertThrows(NullPointerException.class, () -> {
+            new parse_input("(1+1)*1+");
+        });
     }
 }
